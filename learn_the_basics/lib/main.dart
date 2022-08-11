64545cc73b069055746d5a98d94af721d73e6cff
Paulo Filho <pcf.feat@gmail.com>
2022-08-11 16:12:16 -0400

    chore: challenge - create a new app with an appbar theme toggling

--- learn_the_basics/lib/main.dart
+++ learn_the_basics/lib/main.dart
@@ -1,109 +1,40 @@
 import 'package:flutter/material.dart';
-import 'second_page/second_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
-  MyApp({super.key});
+  const MyApp({super.key});
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  bool _themeSwitched = false;
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FirstPage(),
-    );
-  }
-}
-
-class FirstPage extends StatefulWidget {
-  const FirstPage({super.key});
-
-  @override
-  State<FirstPage> createState() => _FirstPageState();
-}
-
-class _FirstPageState extends State<FirstPage> {
-  String buttonName = 'Click';
-  int currentIndex = 0;
-  bool _isClicked = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('App Title'),
+      theme: ThemeData(
+        primarySwatch: _themeSwitched ? Colors.yellow : Colors.pink,
       ),
-      body: Center(
-        child: currentIndex == 0
-            ? Container(
-                color: Colors.red,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        foregroundColor: Colors.white,
-                        backgroundColor: Colors.orange,
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          buttonName = 'Clicked';
-                        });
-                      },
-                      child: Text(buttonName),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (BuildContext context) {
-                              return const SecondPage();
-                            },
-                          ),
-                        );
-                      },
-                      child: Text(buttonName),
-                    ),
-                  ],
-                ),
-              )
-            : GestureDetector(
-                child: _isClicked
-                    ? Image.asset('images/starry_sky.jpg')
-                    : Image.network(
-                        'https://thumbs.dreamstime.com/b/aurora-borealis-acima-em-montanha-de-neve-na-praia-skagsanden-lofoten-nas-ilhas-norway-171472776.jpg'),
-                onTap: () {
-                  setState(() {
-                    _isClicked = !_isClicked;
-                  });
-                },
-              ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My First App'),
+        ),
+        body: Center(
+          child: GestureDetector(
+            child: Image.asset('images/win.png'),
+            onTap: () {
+              setState(() {
+                _themeSwitched = !_themeSwitched;
+              });
+            },
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Settings',
-          ),
-        ],
-        currentIndex: currentIndex,
-        onTap: (int index) {
-          setState(() {
-            currentIndex = index;
-          });
-        },
+        ),
       ),
     );
   }
