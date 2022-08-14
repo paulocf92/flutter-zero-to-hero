cd8926b8028155ca17270b644b47c6c5b76c4be8
Paulo Filho <pcf.feat@gmail.com>
2022-08-14 19:03:58 -0400

    chore: app background color

--- login_app/lib/main.dart
+++ login_app/lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.deepOrangeAccent,
         body: Center(
           child: Image.asset("images/welcome.png"),
         ),
