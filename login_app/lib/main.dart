221bffe13564e28d6c67e524e4966676ffec1cca
Paulo Filho <pcf.feat@gmail.com>
2022-08-14 19:47:14 -0400

    chore: wrap content with safe area to keep it within visible area

--- login_app/lib/main.dart
+++ login_app/lib/main.dart
@@ -13,60 +13,62 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.deepOrangeAccent,
-        body: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Icon(
-                    Icons.coffee_rounded,
-                    color: Colors.white,
-                  ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text(
-                    "WELCOME",
-                    style: TextStyle(
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Icon(
+                      Icons.coffee_rounded,
                       color: Colors.white,
-                      letterSpacing: 2.0,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
                     ),
-                  ),
-                ],
-              ),
-              Center(
-                child: Image.asset("images/welcome.png"),
-              ),
-              OutlinedButton(
-                onPressed: () {
-                  print('Outlined Button');
-                },
-                style: OutlinedButton.styleFrom(
-                  side: const BorderSide(
-                    color: Colors.white,
-                    width: 2.0,
-                  ),
-                  foregroundColor: Colors.white,
-                  minimumSize: const Size(200.0, 40.0),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      "WELCOME",
+                      style: TextStyle(
+                        color: Colors.white,
+                        letterSpacing: 2.0,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
                 ),
-                child: const Text('Sign In'),
-              ),
-              TextButton(
-                onPressed: () {
-                  print('Text Button');
-                },
-                style: TextButton.styleFrom(
-                  foregroundColor: Colors.deepOrangeAccent,
-                  backgroundColor: Colors.white,
-                  minimumSize: const Size(200.0, 40.0),
+                Center(
+                  child: Image.asset("images/welcome.png"),
                 ),
-                child: const Text('Register'),
-              ),
-            ],
+                OutlinedButton(
+                  onPressed: () {
+                    print('Outlined Button');
+                  },
+                  style: OutlinedButton.styleFrom(
+                    side: const BorderSide(
+                      color: Colors.white,
+                      width: 2.0,
+                    ),
+                    foregroundColor: Colors.white,
+                    minimumSize: const Size(200.0, 40.0),
+                  ),
+                  child: const Text('Sign In'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    print('Text Button');
+                  },
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.deepOrangeAccent,
+                    backgroundColor: Colors.white,
+                    minimumSize: const Size(200.0, 40.0),
+                  ),
+                  child: const Text('Register'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
