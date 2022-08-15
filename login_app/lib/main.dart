cb51a7b8acf13bb880ff12b6faa38a77da6ddc45
Paulo Filho <pcf.feat@gmail.com>
2022-08-14 20:16:17 -0400

    chore: challenge done

--- login_app/lib/main.dart
+++ login_app/lib/main.dart
@@ -12,61 +12,133 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.deepOrangeAccent,
+        backgroundColor: Colors.redAccent.shade400,
         body: SafeArea(
           child: Center(
             child: SingleChildScrollView(
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
+                  Center(
+                    child: Image.asset("images/plane.png"),
+                  ),
+                  const SizedBox(height: 50.0),
+                  const Text(
+                    "TRAVEL APP",
+                    style: TextStyle(
+                      color: Colors.white,
+                      letterSpacing: 2.0,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                  const SizedBox(height: 50.0),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Icon(
-                        Icons.coffee_rounded,
+                    children: [
+                      const Icon(
+                        Icons.location_city,
                         color: Colors.white,
+                        size: 30,
                       ),
-                      SizedBox(
-                        width: 10.0,
+                      const SizedBox(
+                        width: 15,
                       ),
-                      Text(
-                        "WELCOME",
-                        style: TextStyle(
-                          color: Colors.white,
-                          letterSpacing: 2.0,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
+                      OutlinedButton(
+                        onPressed: () {
+                          print('Stays');
+                        },
+                        style: OutlinedButton.styleFrom(
+                          side: const BorderSide(
+                            color: Colors.white,
+                            width: 2.0,
+                          ),
+                          foregroundColor: Colors.white,
+                          minimumSize: const Size(200.0, 50.0),
                         ),
+                        child: const Text('Stays'),
                       ),
                     ],
                   ),
-                  Center(
-                    child: Image.asset("images/welcome.png"),
-                  ),
-                  OutlinedButton(
-                    onPressed: () {
-                      print('Outlined Button');
-                    },
-                    style: OutlinedButton.styleFrom(
-                      side: const BorderSide(
+                  const SizedBox(height: 10.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(
+                        Icons.airplanemode_on,
                         color: Colors.white,
-                        width: 2.0,
+                        size: 30,
                       ),
-                      foregroundColor: Colors.white,
-                      minimumSize: const Size(200.0, 40.0),
-                    ),
-                    child: const Text('Sign In'),
+                      const SizedBox(
+                        width: 15,
+                      ),
+                      OutlinedButton(
+                        onPressed: () {
+                          print('Flight');
+                        },
+                        style: OutlinedButton.styleFrom(
+                          side: const BorderSide(
+                            color: Colors.white,
+                            width: 2.0,
+                          ),
+                          foregroundColor: Colors.white,
+                          minimumSize: const Size(200.0, 50.0),
+                        ),
+                        child: const Text('Flight'),
+                      ),
+                    ],
                   ),
-                  TextButton(
-                    onPressed: () {
-                      print('Text Button');
-                    },
-                    style: TextButton.styleFrom(
-                      foregroundColor: Colors.deepOrangeAccent,
-                      backgroundColor: Colors.white,
-                      minimumSize: const Size(200.0, 40.0),
-                    ),
-                    child: const Text('Register'),
+                  const SizedBox(height: 10.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(
+                        Icons.car_rental,
+                        color: Colors.white,
+                        size: 30,
+                      ),
+                      const SizedBox(
+                        width: 15,
+                      ),
+                      OutlinedButton(
+                        onPressed: () {
+                          print('Cars');
+                        },
+                        style: OutlinedButton.styleFrom(
+                          side: const BorderSide(
+                            color: Colors.white,
+                            width: 2.0,
+                          ),
+                          foregroundColor: Colors.white,
+                          minimumSize: const Size(200.0, 50.0),
+                        ),
+                        child: const Text('Cars'),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 10.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(
+                        Icons.directions_boat,
+                        color: Colors.white,
+                        size: 30,
+                      ),
+                      const SizedBox(
+                        width: 15,
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          print('All-inclusive Vacations');
+                        },
+                        style: OutlinedButton.styleFrom(
+                          foregroundColor: Colors.redAccent.shade400,
+                          backgroundColor: Colors.white,
+                          minimumSize: const Size(200.0, 50.0),
+                        ),
+                        child: const Text('All-inclusive Vacations'),
+                      ),
+                    ],
                   ),
                 ],
               ),
