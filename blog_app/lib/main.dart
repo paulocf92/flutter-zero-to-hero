c37f1597c777b97c586ba888cdc1e26045d682fd
Paulo Filho <pcf.feat@gmail.com>
2022-08-18 13:42:34 -0400

    chore: delegate scaffold to a separate page

--- blog_app/lib/main.dart
+++ blog_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,52 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(),
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        body: SafeArea(
-          child: Center(
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(height: 20),
-                  const Text(
-                    "WELCOME TO THE BLOG",
-                    style: TextStyle(
-                        letterSpacing: 2,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 25),
-                  ),
-                  Image.asset("images/welcome.png"),
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blueGrey[900],
-                      minimumSize: const Size(250, 45),
-                      elevation: 0,
-                    ),
-                    child: const Text('Sign In'),
-                  ),
-                  const SizedBox(height: 10),
-                  TextButton(
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.blueGrey[100],
-                      foregroundColor: Colors.blueGrey[900],
-                      minimumSize: const Size(250, 45),
-                      elevation: 0,
-                    ),
-                    onPressed: () {
-                      print("Register");
-                    },
-                    child: const Text("Register"),
-                  ),
-                  const SizedBox(height: 20),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
+      home: const LoginPage(),
     );
   }
 }
