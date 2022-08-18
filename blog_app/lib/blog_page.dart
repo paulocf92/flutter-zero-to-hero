56ccd75678416e796d02f3e8d31c763293bf6a36
Paulo Filho <pcf.feat@gmail.com>
2022-08-18 14:39:33 -0400

    chore: simplify other posts

--- blog_app/lib/blog_page.dart
+++ blog_app/lib/blog_page.dart
@@ -29,74 +29,8 @@ class BlogPage extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               widgetPost1(),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20.0),
-                  color: Colors.black.withOpacity(0.05),
-                ),
-                height: 200.0,
-                width: double.infinity,
-                child: Image.asset(
-                  'images/house.png',
-                  fit: BoxFit.cover,
-                ),
-              ),
-              const SizedBox(height: 10),
-              const Text(
-                'How to purchase a property in 5 steps',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
-                ),
-              ),
-              const Text(
-                'You must acknowledge this before shopping',
-                style: TextStyle(
-                  fontSize: 16,
-                ),
-              ),
-              const SizedBox(
-                width: 50.0,
-                child: Divider(
-                  color: Colors.black,
-                  thickness: 5,
-                ),
-              ),
-              const SizedBox(height: 20.0),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20.0),
-                  color: Colors.black.withOpacity(0.05),
-                ),
-                height: 200.0,
-                width: double.infinity,
-                child: Image.asset(
-                  'images/apps.png',
-                  fit: BoxFit.cover,
-                ),
-              ),
-              const SizedBox(height: 10),
-              const Text(
-                '7 Apps to increase productivity',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
-                ),
-              ),
-              const Text(
-                'These apps will boost your daily tasks',
-                style: TextStyle(
-                  fontSize: 16,
-                ),
-              ),
-              const SizedBox(
-                width: 50.0,
-                child: Divider(
-                  color: Colors.black,
-                  thickness: 5,
-                ),
-              ),
-              const SizedBox(height: 20.0),
+              widgetPost2(),
+              widgetPost3(),
               TextButton(
                 style: TextButton.styleFrom(
                   foregroundColor: Colors.blueGrey[900],
